931a9b47182afc440debcd06587af73d07a49130
Aman Parmar <amanparmar9171.com>
2022-11-27 16:42:41 +0530

    update

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(session({
     resave:false,
     saveUninitialized:false,
     cookie:{
-        secure:process.env.NODE_ENV ==="development"? false : true,
-        httpOnly:process.env.NODE_ENV ==="development"? false : true,
-        sameSite:process.env.NODE_ENV ==="development"? false : "none"
+        secure:process.env.NODE_ENV ==="development" ? false : true,
+        httpOnly:process.env.NODE_ENV ==="development" ? false : true,
+        sameSite:process.env.NODE_ENV ==="development" ? false : "none"
     }
  
 }))

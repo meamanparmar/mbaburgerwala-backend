19d72d3c6a2bd74cfc8fda7fb824b7463e152048
Aman Parmar <amanparmar9171.com>
2022-11-27 12:10:22 +0530

    update

--- routes/user.js
+++ routes/user.js
@@ -13,14 +13,20 @@ router.get(
 )
 
 
+// router.get(
+//     "/login",
+//     passport.authenticate("google"),{
+//         // successRedirect:prcess.env.FRONTEND_URL
+//         successRedirect:process.env.FRONTEND_URL
+//     }
+    
+// )
 router.get(
     "/login",
-    passport.authenticate("google"),{
-        // successRedirect:prcess.env.FRONTEND_URL
-        successRedirect:process.env.FRONTEND_URL
-    }
-    
-)
+    passport.authenticate("google", {
+      successRedirect: process.env.FRONTEND_URL,
+    })
+  );
 
 router.get("/me", isAuthenticated ,myProfile)
 router.get("/logout",logout)

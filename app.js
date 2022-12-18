79058ea0cb4e9c6f652726fb8410078cb95083e3
meamanparmar <parmaraman9171@gmail.com>
2022-12-18 17:22:55 +0530

    first commit

--- app.js
+++ app.js
@@ -45,7 +45,11 @@ app.use(cors({
     methods:["GET","POST","PUT","DELETE"],
     
 }))
-
+app.use(function (req,res,next){
+    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Headers","Origin , X-Requested-With , Content-Type ,Accept")
+    next()
+  })
 
 app.use(passport.authenticate("session"))
 app.use(passport.initialize())

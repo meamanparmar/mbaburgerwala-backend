295711557526bb9e13b1405d784df9f20e029c57
meamanparmar <parmaraman9171@gmail.com>
2023-01-10 11:06:57 +0530

    first commit

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.use(cors({
 }))
 app.use(function (req,res,next){
     res.header("Access-Control-Allow-Origin","http://localhost:3000")
-    res.header("Access-Control-Allow-Origin","https://mbaburgerwalaapi.onrender.com/api/v1/me")
-    res.header("Access-Control-Allow-Origin","*")
+    // res.header("Access-Control-Allow-Origin","https://mbaburgerwalaapi.onrender.com/api/v1/me")
+    // res.header("Access-Control-Allow-Origin","*")
     res.header("Access-Control-Allow-Headers","Origin , X-Requested-With , Content-Type ,Accept")
     res.header("Access-Control-Allow-Credentials", true);
     next()
